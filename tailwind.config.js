ed3ee00b3c9b8a04c78f3ddc390ff1fe0e371bbb
vpullakhandam <124179668+vpullakhandam@users.noreply.github.com>
2024-12-25 12:45:19 -0500

    installed tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#3B82F6', // blue-500
+          hover: '#2563EB', // blue-600
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+
